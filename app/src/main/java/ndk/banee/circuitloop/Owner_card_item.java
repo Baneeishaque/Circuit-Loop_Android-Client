d4dd752708e438842c2e2a8545b950f9477674ee
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-14 21:54:24 +0530

    ownerItemDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Owner_card_item.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Owner_card_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_card_item);
+    }
+}
