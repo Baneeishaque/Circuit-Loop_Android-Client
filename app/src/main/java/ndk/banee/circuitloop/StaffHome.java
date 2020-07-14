818ac6068de1c8997d4a71c792ee0836dcbf6e33
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-14 22:30:17 +0530

    StaffHomeDone

---
+++ app/src/main/java/ndk/banee/circuitloop/StaffHome.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_home);
+    }
+}
