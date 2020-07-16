00cbc8b6455aed9c67fc8e26b2c37a3900a134c1
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-16 10:16:10 +0530

    addProductDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Add_products.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_products);
+    }
+}
