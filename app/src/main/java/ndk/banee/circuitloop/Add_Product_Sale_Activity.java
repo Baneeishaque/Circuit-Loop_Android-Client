4475dff8f2c59faaba16d8b4fe19bed36c3b439c
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-16 20:14:09 +0530

    AddProductSaleActivityDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Add_Product_Sale_Activity.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Product_Sale_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__product__sale_);
+    }
+}
