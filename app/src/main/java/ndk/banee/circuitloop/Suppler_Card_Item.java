2c44a48ad10080c3a7b6b8c9f53f59a05a18f312
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-16 16:31:41 +0530

    SupplerCardItemDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Suppler_Card_Item.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suppler_Card_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suppler__card__item);
+    }
+}
