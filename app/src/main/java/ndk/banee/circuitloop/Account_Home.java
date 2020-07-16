10e107d0e5e4d9b223634c26f38f1ae74dabb37b
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-16 16:10:47 +0530

    AccountHomeDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Account_Home.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Account_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account__home);
+    }
+}
