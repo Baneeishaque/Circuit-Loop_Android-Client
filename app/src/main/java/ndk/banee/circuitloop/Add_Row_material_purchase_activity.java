1e047b526fc56632f46bf2b5c085189b4af03ef9
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-17 13:13:51 +0530

    addRowMaterialActivityDone

---
+++ app/src/main/java/ndk/banee/circuitloop/Add_Row_material_purchase_activity.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Row_material_purchase_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__row_material_purchase_activity);
+    }
+}
