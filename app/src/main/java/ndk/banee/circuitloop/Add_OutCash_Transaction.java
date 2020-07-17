3cdbba1a98d34aff9164d31546957eb60787be11
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-17 21:00:55 +0530

    add_OutCash_Done

---
+++ app/src/main/java/ndk/banee/circuitloop/Add_OutCash_Transaction.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_OutCash_Transaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__out_cash__transaction);
+    }
+}
