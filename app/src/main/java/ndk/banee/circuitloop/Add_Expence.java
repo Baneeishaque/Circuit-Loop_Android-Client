fd61fda4c75f161471b0cfec9470cb734c41146e
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-17 19:24:16 +0530

    add_Expense_Done

---
+++ app/src/main/java/ndk/banee/circuitloop/Add_Expence.java
@@ -0,0 +1,14 @@
+package ndk.banee.circuitloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Expence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__expence);
+    }
+}
